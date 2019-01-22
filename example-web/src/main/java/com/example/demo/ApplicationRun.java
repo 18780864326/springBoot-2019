99c440788b7c6a3fb041f2bbc1d478295caa6803
18780864326 <35372708+18780864326@users.noreply.github.com>
2019-01-22 16:40:16 +0800

    1

--- example-web/src/main/java/com/example/demo/ApplicationRun.java
+++ example-web/src/main/java/com/example/demo/ApplicationRun.java
@@ -8,5 +8,4 @@ public class ApplicationRun {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationRun.class, args);
 	}
-
 }
--- example-web/src/main/java/com/example/demo/system/login/controller/LoginController.java
+++ example-web/src/main/java/com/example/demo/system/login/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.demo.common.dto.MessageInfo;
 import com.example.demo.entity.TestUser;
 import com.example.demo.system.login.dto.LoginDto;
 import com.example.demo.system.login.service.LoginService;
@@ -20,8 +21,16 @@ public class LoginController {
 	
 	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public String loginSystem(LoginDto loginDto) {
-		TestUser user  =	loginService.findUserInfoByDto(loginDto);
-		String str = JSONObject.toJSONString(user).toString();
+			TestUser user  =	loginService.findUserInfoByDto(loginDto);
+			MessageInfo msg = new MessageInfo();
+			if(user != null) {
+				msg.setStatus(1);
+				msg.setMsg("登录成功");
+			}else {
+				msg.setStatus(0);
+				msg.setMsg("登录失败，账户或者密码错误");
+			}
+			String str = JSONObject.toJSONString(msg).toString();
 		return str;	
 	}
 }
