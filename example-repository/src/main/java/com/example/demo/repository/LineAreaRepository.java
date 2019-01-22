af31392005710410b6d97403668201eeeaad26bb
xzl <xiaozelin_zx@163.com>
2019-01-22 15:45:16 +0800

    1

---
+++ example-repository/src/main/java/com/example/demo/repository/LineAreaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.baseRepository.first.BaseRepository;
+
+import com.example.demo.entity.LineArea;
+
+public interface LineAreaRepository extends BaseRepository<LineArea,Long>{
+
+}
---
+++ example-repository/src/main/java/com/example/demo/repository/MonitorAreaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.baseRepository.first.BaseRepository;
+
+import com.example.demo.entity.MonitorArea;
+
+public interface MonitorAreaRepository extends BaseRepository<MonitorArea,Long>{
+
+}
